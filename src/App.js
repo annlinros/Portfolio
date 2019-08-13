5286dbc087f22f63dc6a71792b6073f77863d184
annlinros <annlinros@gmail.com>
2019-08-13 19:31:58 +0530

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {  Route, Switch } from "react-router-dom";
+
 import './App.css';
+import Navigation from './Components/Navigation'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <Switch>
+        <Route>
+          {/* <About /> */}
+        </Route>
+        <Route>
+          {/* <Projects /> */}
+        </Route>
+      </Switch>
     </div>
   );
 }
