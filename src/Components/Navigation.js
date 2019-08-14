09fd932fde90e0f1734a80ed614900a469d8e244
annlinros <annlinros@gmail.com>
2019-08-14 19:34:00 +0530

    Create Navigation.js

---
+++ src/Components/Navigation.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+
+export default function Navigation() {
+  return (
+    <Navbar bsPrefix="navbar" collapseOnSelect expand="lg">
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+      />
+      <Navbar.Collapse  id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link bsPrefix="nav-link" as={Link} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link bsPrefix="nav-link" as={Link} to="/projects">
+            Projects
+          </Nav.Link>
+          <Nav.Link bsPrefix="nav-link" as={Link} to="/resume">
+            Resume
+          </Nav.Link>
+          <Nav.Link bsPrefix="nav-link" as={Link} to="/contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
