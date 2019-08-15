4aa3b71dc42978468cda97d588f14647e1fbd69c
annlinros <annlinros@gmail.com>
2019-08-15 10:24:53 +0530

    classname changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDoubleDown, faCode } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
 import {
   faGithub,
   faCodepen,
@@ -12,7 +13,7 @@ export default function Home() {
   return (
     <Container fluid className="home d-flex">
       <ButtonGroup
-        className="home-sidebar d-flex flex-column  align-items-center"
+        className="sidebar d-flex flex-column  align-items-center"
         vertical
         btn-group
       >
@@ -22,7 +23,7 @@ export default function Home() {
       </ButtonGroup>
       <Container
         fluid
-        className="home-main d-flex flex-column align-items-center justify-content-start"
+        className="main d-flex flex-column align-items-center justify-content-start"
       >
         <Container>
           <h1>Hi, I'm Anna</h1>
@@ -31,7 +32,9 @@ export default function Home() {
             <span className="sub-text"> front-end web developer</span>
           </p>
         </Container>
-        <FontAwesomeIcon className="arrowBtn" icon={faAngleDoubleDown} />
+        <Link to="/about">
+          <FontAwesomeIcon className="arrowBtn" icon={faAngleDoubleDown} />
+        </Link>
       </Container>
     </Container>
   );
