0ceac0bd2c7fd84cfa753532534d5a0ae3639628
annlinros <annlinros@gmail.com>
2019-08-15 12:27:04 +0530

    Update Skills.js

--- src/Components/About/Skills.js
+++ src/Components/About/Skills.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
-// import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
