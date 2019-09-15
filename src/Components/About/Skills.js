932c44da11bc2487fc303340d34db99a4cc14d78
Anna <annlinros@gmail.com>
2019-09-15 22:05:26 +0200

    Update Skills.js

--- src/Components/About/Skills.js
+++ src/Components/About/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
@@ -12,31 +12,23 @@ import {
 
 export default function Skills() {
   return (
-    <Container fluid className="about d-flex">
-      <Container
-        className="sidebar d-flex flex-column  align-items-center"
-        vertical
-      />
+    <Container fluid className="skills d-flex">
+      <Container className="sidebar"/>
       <Container
         fluid
-        className="skills d-flex flex-column  align-items-center justify-content-center"
+        className="d-flex flex-column  align-items-center justify-content-center"
       >
-        <h1 className="sub-heading ">Skills</h1>
-        <Row>
-          <Col className="text-center">
-            <FontAwesomeIcon className="skills-icon html" icon={faHtml5} />
-            <FontAwesomeIcon
-              className="skills-icon css"
-              icon={faCss3Alt}
-            />{" "}
-            <FontAwesomeIcon className="skills-icon js" icon={faJs} />{" "}
-            <FontAwesomeIcon
-              className="skills-icon bootstrap"
-              icon={faBootstrap}
-            />{" "}
-            <FontAwesomeIcon className="skills-icon react" icon={faReact} />{" "}
-          </Col>
-        </Row>
+        <h1 className="sub-heading mb-5">Skills</h1>
+        <Container>
+          <FontAwesomeIcon className="skills-icon html" icon={faHtml5} />
+          <FontAwesomeIcon className="skills-icon css" icon={faCss3Alt} />{" "}
+          <FontAwesomeIcon className="skills-icon js" icon={faJs} />{" "}
+          <FontAwesomeIcon
+            className="skills-icon bootstrap"
+            icon={faBootstrap}
+          />{" "}
+          <FontAwesomeIcon className="skills-icon react" icon={faReact} />{" "}
+        </Container>
       </Container>
     </Container>
   );
