6df7ebbe913c4cec62c4030c025113eeace24764
Anna <annlinros@gmail.com>
2019-09-17 11:06:48 +0200

    Add links to the components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import {  Route, Switch } from "react-router-dom";
-
-import './App.css';
-import Navigation from './Components/Navigation'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navigation from "./Components/Navigation";
+import Home from "./Components/Home";
+import About from "./Components/About/About";
+import Skills from "./Components/Skills";
+import Contact from "./Components/Contact";
+import Projects from "./Components/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Switch>
-        <Route>
-          {/* <About /> */}
+        <Route exact path="/" component={Home}>
+          <Home />
         </Route>
-        <Route>
-          {/* <Projects /> */}
+        <Route path="/about" component={About}>
+          <About />
+        </Route>
+        <Route path="/skills" component={Skills}>
+          <Skills />
+        </Route>
+        <Route path="/projects" component={Projects}>
+          <Projects />
+        </Route>
+        <Route exact path="/contact" component={Contact}>
+          <Contact />
         </Route>
       </Switch>
     </div>
